eb95a6ebbf8cc6085f9a052c3dbcacbc162917c4
Gray007 <cmzlelicanin@gmail.com>
2021-08-24 12:07:34 +0200

    course information - 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,11 +8,23 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+// Part component
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercises}
     </p>
   )
 }
+    
 
 const Total = (props) => {
   return (
@@ -33,10 +45,11 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Header course={course} />
+      <Content 
+        part1={part1} part2={part2} part3={part3} 
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+      />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
