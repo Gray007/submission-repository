4dd5a141460cf648ceccaa5d6ab3f96805debdfe
Gray007 <cmzlelicanin@gmail.com>
2021-08-29 08:39:18 +0200

    course information - 2.1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,57 +1,79 @@
 import React from 'react'
 
-const Header = ({ course }) => <h1>{course.name}</h1>
-
-const Content = ({ course }) => (
-    <div>
-      <Part part={course.parts[0].name} exercises={course.parts[0].exercises}/>
-      <Part part={course.parts[1].name} exercises={course.parts[1].exercises}/>
-      <Part part={course.parts[2].name} exercises={course.parts[2].exercises}/>
-    </div>
-  )
-
-const Part = ({ part, exercises }) => (
-    <p>
-      {part} {exercises}
-    </p>
-  )
-    
-
-const Total = ({ course }) => {
-  const totalExercises =  course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+const Course = ( {course} ) => {
   return (
-    <p>
-      Number of exercises {totalExercises}
-    </p>
+    <div>
+      <Header key={course.id} text={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
   )
 }
 
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Content = ({ parts }) => (
+    <ul>
+      {parts.map(part => 
+      <Part key={part.id} part={part} />
+       )}
+    </ul>
+  )
+
+const Part = ({ part }) => <li>{part.name} {part.exercises}</li>
+
+// const Total = ({ course }) => {
+//   const totalExercises =  course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+//   return (
+//     <p>
+//       Number of exercises {totalExercises}
+//     </p>
+//   )
+// }
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
+        exercises: 14,
+        id: 3
+      },
     ]
+    // ,
+  // id: 2,
+  // name: 'Full Stack application development',
+  // parts: [
+  //     {
+  //       name: 'Fundamentals of React 2',
+  //       exercises: 77,
+  //       id: 1
+  //     },
+  //     {
+  //       name: 'Using props to pass data 2',
+  //       exercises: 77,
+  //       id: 2
+  //     },
+  //     {
+  //       name: 'State of a component 2',
+  //       exercises: 77,
+  //       id: 3
+  //     }
+  //   ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total  course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
