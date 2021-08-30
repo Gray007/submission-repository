2d832bd2e57bf29afdfb9149a476fd14e309cb9b
Gray007 <cmzlelicanin@gmail.com>
2021-08-30 13:51:59 +0200

    the phonebook - 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,7 @@
 import React, { useState } from 'react'
-import Heading from './components/Heading'
+import Search from './components/Search'
+import AddContact from './components/AddContact'
 import List from './components/List'
-
-const Search = ({ newFilter, handleFilterChange}) => {
-  return (
-    <div>
-      <Heading text={'Phonebook'}/>
-      <Input text={'Filter by name'} newValue={newFilter} handleChange={handleFilterChange} />
-    </div>
-  )
-}
-
-const AddContact = ({ addNameNumber, newName, newNumber, handleNumberChange, handleNameChange }) => {
-  return (
-  <div>
-    <Heading text={'Add Contact'}/>
-      <form onSubmit={addNameNumber}>
-        <Input text={'Name'} newValue={newName} handleChange={handleNameChange} />
-        <Input text={'Number'} newValue={newNumber} handleChange={handleNumberChange} />
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-  </div>
-  )
-}
-
-const Input = ({ text, newValue, handleChange}) => (
-  <div>
-    {text}: 
-    <input 
-      value={newValue}
-      onChange={handleChange}
-    />
-  </div>
-)
 
 const App = () => {
   const [ persons, setPersons ] = useState([
--- part2/phonebook/src/components/List.js
+++ part2/phonebook/src/components/List.js
@@ -2,22 +2,22 @@ import React from 'react'
 import Heading from './Heading'
 
 const List = ({ persons, filter }) => {
-    let filterRegex = new RegExp(filter, "gi")
+let filterRegex = new RegExp(filter, "gi")
     return (
-      <div>
-        <Heading text={'Numbers'}/>
-        <ul>
-          {persons
-            .filter(person => person.name
-            .match(filterRegex))
-            .map(person => 
-          <Entry key={person.name} person={person} />
-          )}
-        </ul>
-      </div>
+        <div>
+            <Heading text={'Numbers'}/>
+            <ul>
+                {persons
+                .filter(person => person.name
+                .match(filterRegex))
+                .map(person => 
+                <Entry key={person.name} person={person} />
+                )}
+            </ul>
+        </div>
     )
-  }
-  
-  const Entry = ({ person }) => <li>{person.name} : {person.number} </li>
+}
 
-  export default List
+const Entry = ({ person }) => <li>{person.name} : {person.number} </li>
+
+export default List
