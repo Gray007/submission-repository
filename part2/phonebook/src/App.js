ee7ddec9466a7d86395dcbeaddc7dac3178fe791
Gray007 <cmzlelicanin@gmail.com>
2021-08-30 10:16:38 +0200

    the phonebook - 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,30 @@
 import React, { useState } from 'react'
+import Heading from './components/Heading'
+import List from './components/List'
+
+const AddContact = ( {addName, newName, handleNameChange} ) => {
+  return (
+  <div>
+    <Heading text={'Phonebook'}/>
+      <form onSubmit={addName}>
+        <Input name={'Name'} newName={newName} handleNameChange={handleNameChange}/>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  </div>
+  )
+}
+
+const Input = ({ name, newName, handleNameChange}) => (
+  <div>
+    {name}: 
+    <input 
+      value={newName}
+      onChange={handleNameChange}
+    />
+  </div>
+)
 
 const App = () => {
   const [ persons, setPersons ] = useState([
@@ -6,19 +32,25 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    const personsObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personsObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+      <AddContact addName={addName} newName={newName} handleNameChange={handleNameChange}/>
+      <List persons={persons} />   
     </div>
   )
 }
